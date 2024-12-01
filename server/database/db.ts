051189f1c9878ca33e4dc77933a89c5f9d59b3f8
Crazibeat <crazibeatdeveloper@gmail.com>
2024-12-01 15:25:15 +0100

    Update

--- server/database/db.ts
+++ server/database/db.ts
@@ -2,23 +2,24 @@ import { Sequelize } from "sequelize";
 import { initModels } from "./models/init-models";
 import pg from "pg";
 import mysql from "mysql2";
+import config from "./db_config";
 
 let sequelizeInstance: Sequelize | null = null;
 
 const getSequelizeInstance = async () => {
   if (!sequelizeInstance) {
     sequelizeInstance = new Sequelize({
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT ?? "3306"),
-      username: process.env.DATABASE_USER ?? "root",
-      database: process.env.DATABASE_NAME,
-      password: process.env.DATABASE_PASS ?? "",
-      dialect: process.env.DATABASE_DIALECT ?? ("mysql" as any),
-      dialectModule: process.env.DATABASE_DIALECT == "mysql" ? mysql : pg,
-      ssl: process.env.DATABASE_SSL == "true",
+      host: config.host,
+      port: config.port,
+      username: config.username,
+      database: config.database,
+      password: config.password,
+      dialect: config.dialect,
+      dialectModule: config.dialect == "mysql" ? mysql : pg,
+      dialectOptions: config.dialectOptions,
       logging: (sql, timing) => {
-        if (process.env.LOG_DATABASE_QUERIES ?? true) {
-          // console.info(sql);
+        if (process.env.LOG_DATABASE_QUERIES == "true") {
+          console.info(sql);
         }
       },
     });
--- server/database/db_config.js
+++ server/database/db_config.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
 
+/** @type {import('sequelize').Options} */
 const configs = {
-  development: {
-    dialect: process.env.DATABASE_DIALECT,
-    host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
-    port: parseInt(process.env.DATABASE_PORT),
-    ssl: process.env.DATABASE_SSL == "true"
-  },
-  production: {
-    dialect: process.env.DATABASE_DIALECT,
-    host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
-    port: parseInt(process.env.DATABASE_PORT),
-    ssl: process.env.DATABASE_SSL == "true",
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  port: parseInt(process.env.DATABASE_PORT),
+  dialectOptions: {
+    ssl: process.env.DATABASE_SSL == "true" && {
+      rejectUnauthorized: false,
+    },
   },
 };
 
