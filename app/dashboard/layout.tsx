d8ca6a51e39eb14748063d88b082f7e8d6cc4cc6
Crazibeat <crazibeatdeveloper@gmail.com>
2025-06-04 11:35:08 +0100

    Redirect to verification if user isn't verified, redirect to login if user isn't found

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,12 +4,24 @@ import { Shape2 } from "@/components/shapes/shape_2";
 import { Container } from "@radix-ui/themes";
 import { headers } from "next/headers";
 import { CryptoConvertProvider } from "context/CryptoConvertContext";
+import { authUser } from "@/actions/authUser";
+import { fetchUser } from "@/fetch/fetch_user";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  var user_id = (await authUser()).user_id;
+  var user = await fetchUser(user_id, { force: true });
+
+  if (!user) {
+    redirect("/auth/login");
+  } else if (user.ssnStatus !== "verified" || user.idDocStatus !== "verified") {
+    redirect("/auth/verification");
+  }
+
   return (
     <div>
       {/* <Shape1 className={"fixed bottom-0 left-[-100px] -z-10 w-[300px]"} /> */}
