1923459c7e33936cba4ca1fb3aba9dca488aa3b3
Crazibeat <crazibeatdeveloper@gmail.com>
2025-07-02 14:44:28 +0100

    feat: Enable server actions and increase body size limit
    This commit enables Next.js server actions and increases the body size limit to 10mb.
    
    - Enabled the `serverActions` experimental feature in `next.config.mjs`.
    - Set the `bodySizeLimit` to "10mb" for server actions.

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
 };
