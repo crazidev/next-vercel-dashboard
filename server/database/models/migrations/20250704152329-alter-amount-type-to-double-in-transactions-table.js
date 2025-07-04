87a227f669b3d911d5241ecf5ef8f2cfa71f7b53
Crazibeat <crazibeatdeveloper@gmail.com>
2025-07-04 16:57:48 +0100

    Refactor: Change amount type to double in transactions table
    This commit refactors the `transactions` table to change the `amount` column type from `INTEGER` to `DOUBLE`. This allows for storing decimal values for transaction amounts.
    
    - Changed the `amount` column type in the `transactions` table to `DOUBLE` in both the `up` and `down` migrations.

--- server/database/models/migrations/20250704152329-alter-amount-type-to-double-in-transactions-table.js
+++ server/database/models/migrations/20250704152329-alter-amount-type-to-double-in-transactions-table.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.update("transactions", "amount", {
+    await queryInterface.changeColumn("transactions", "amount", {
       type: Sequelize.DOUBLE,
       allowNull: false,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.update("transactions", "amount", {
+    await queryInterface.changeColumn("transactions", "amount", {
       type: Sequelize.INTEGER,
       allowNull: false,
     });
